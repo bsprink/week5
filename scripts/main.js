96f37ad281388475faea869e792dfdda7902869b
bsprink <bsprink@comcast.net>
2017-02-13 18:21:31 -0600

    added some vector

--- scripts/main.js
+++ scripts/main.js
@@ -8,13 +8,15 @@ $(document).scroll(function(){
 	$("#frontClouds").css("left",String($(document).scrollTop() -70) + "px" );
 	$("#middleClouds").css("left",String($(document).scrollTop() * .5) + "px" );
 	$("#backClouds").css("left",String($(document).scrollTop() * .2) + "px" );
+	$("#me").css("left",String($(document).scrollTop() * .2) + "px" );
 });
 
 $(document).mousemove(function(p){
 	console.log(p.pageX + "," +p.pageY);
 	if(p.pageX > 100 && p.pageX < 900){
-		$("#frontClouds").css("left",String(p.pageX -800) + "px" );
-		$("#middleClouds").css("left",String(p.pageX * .5 ) + "px" );
-		$("#backClouds").css("left",String(p.pageX * .2) + "px" );
+		$("#frontClouds").css("left",String(p.pageX * -.1) + "px" );
+		$("#middleClouds").css("left",String(p.pageX * .0 ) + "px" );
+		$("#backClouds").css("left",String(p.pageX * .0) + "px" );
+		$("#me").css("left",String(p.pageX * .100) + "px" );
 	}
 });
